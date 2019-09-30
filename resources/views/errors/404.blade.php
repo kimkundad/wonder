00452df0fb912b792003ad2d282a968d23175aa1
kimkundad <kim.kundad@gmail.com>
2019-09-30 17:29:24 +0700

    golf

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -16,7 +16,7 @@
 
 <div class="theme-hero-area theme-hero-area-full">
   <div class="theme-hero-area-bg-wrap">
-    <div class="theme-hero-area-bg" style="background-image:url({{asset('./assets/images/bg_page.jpg')}});"></div>
+    <div class="theme-hero-area-bg" style="background-image:url({{asset('/assets/home/img/bg_page.jpg')}});"></div>
     <div class="theme-hero-area-mask theme-hero-area-mask-dark"></div>
     <div class="theme-hero-area-inner-shadow"></div>
   </div>
