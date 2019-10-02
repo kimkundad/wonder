a2d1395f77b30b5fff599500f66edd1aaf125f62
kimkundad <kim.kundad@gmail.com>
2019-10-02 18:09:09 +0700

    golf

--- resources/views/unlock1/index.blade.php
+++ resources/views/unlock1/index.blade.php
@@ -331,18 +331,13 @@ AcmeTrader กลุ่มสุดยอดนักเทรดที่ก่
 <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 
 <div id="fb-root"></div>
-<script>
-/*setTimeout(function(){
-  window.location = "{{url('/unlock_events')}}";
-}, 100); */
-
-(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = 'https://connect.facebook.net/th_TH/sdk.js#xfbml=1&version=v2.12&appId=148045139197033&autoLogAppEvents=1';
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
+  <script>(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.0";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));</script>
 
 
 <script>
