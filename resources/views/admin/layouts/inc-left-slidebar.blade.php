da47330163d9ed76c2a83b5b1cb758ed5cbdebe5
kimkundad <kim.kundad@gmail.com>
2019-10-04 16:07:34 +0700

    golf

--- resources/views/admin/layouts/inc-left-slidebar.blade.php
+++ resources/views/admin/layouts/inc-left-slidebar.blade.php
@@ -13,14 +13,14 @@
   <div class="sidebar-wrapper">
     <ul class="nav">
 
-      <li class="nav-item {{ (Request::is('admin/dashboard*') ? 'active' : '') }} {{ (Request::is('admin/search_student*') ? 'active' : '') }} ">
+      <li class="nav-item {{ (Request::is('admin/dashboard*') ? 'active' : '') }} ">
         <a class="nav-link" href="{{url('admin/dashboard')}}">
           <i class="material-icons">dashboard</i>
           <p>Dashboard</p>
         </a>
       </li>
 
-      <li class="nav-item {{ (Request::is('admin/users*') ? 'active' : '') }} ">
+      <li class="nav-item {{ (Request::is('admin/users*') ? 'active' : '') }} {{ (Request::is('admin/search_student*') ? 'active' : '') }}">
         <a class="nav-link" href="{{url('admin/users')}}">
           <i class="material-icons">person</i>
           <p>รายชื่อลูกค้า</p>
